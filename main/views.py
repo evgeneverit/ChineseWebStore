5db06ae5dacc16ec23fa47fc786199fd6ed05fac
Evgeny Alekseev <evgeneverit@gmail.com>
2024-11-24 13:28:25 +0400

    After addind fixtures

--- main/views.py
+++ main/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         'title': 'China - Главная',
-        'content': 'Онлайн магазин товаров из Китая'
+        'content': 'Онлайн магазин товаров из Китая',
+        'categories': categories
     }
 
     return render(request, 'main/index.html', context)
